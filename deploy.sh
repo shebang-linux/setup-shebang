779bccc15e7f6cd66404aa2152edc72216407812
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-09-15 12:46:27 +0000

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -48,8 +48,8 @@ sed -i -e s"/\#NoExtract.*/NoExtract = usr\/share\/doc\/* usr\/share\/gtk-doc\/*
 sed -i -e s'/\/usr\/bin/\/sbin/'g /etc/makepkg.conf
 sed -i -e s'/curl -qgb.*/curl -fSL --proto=https --tlsv1.3 --progress-bar -o %o %u'\''/'g /etc/makepkg.conf
 sed -i -e s'/^CHOST.*/CHOST="x86_64-linux-musl"/'g /etc/makepkg.conf
-sed -i -e '/^CFLAGS=/,/"/c\CFLAGS="-march=native -mtune=native -Os -flto -Wl,-flto -pipe \\\n\t-fomit-frame-pointer -momit-leaf-frame-pointer \\\n\t-fno-exceptions -fno-rtti \\\n\t-fno-plt -fno-semantic-interposition \\\n\t-ffunction-sections -fdata-sections"' /etc/makepkg.conf
-sed -i -e s'/^CXXFLAGS=.*/CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_PARALLEL"/'g /etc/makepkg.conf
+sed -i -e '/^CFLAGS=/,/"/c\CFLAGS="-march=native -mtune=native -Os -flto -Wl,-flto -pipe \\\n\t-fomit-frame-pointer -momit-leaf-frame-pointer \\\n\t-fno-exceptions -fno-plt \\\n\t-fno-semantic-interposition \\\n\t-ffunction-sections -fdata-sections"' /etc/makepkg.conf
+sed -i -e s'/^CXXFLAGS=.*/CXXFLAGS="$CFLAGS fno-rtti -Wp,-D_GLIBCXX_PARALLEL"/'g /etc/makepkg.conf
 sed -i -e s'/^LDFLAGS=.*/LDFLAGS="-Os -Wl,-flto -Wl,--sort-common -Wl,--as-needed -Wl,--no-undefined -Wl,--gc-sections -Wl,-z,now -s"/'g /etc/makepkg.conf
 sed -i -e s'/^DEBUG_CFLAGS=.*/DEBUG_CFLAGS="-g0 -s"/'g /etc/makepkg.conf
 sed -i -e "s/-j.*/-j$(expr $(nproc) - 1) -l$(nproc)\"/;s/^#MAKEFLAGS/MAKEFLAGS/;s/^#RUSTFLAGS/RUSTFLAGS/" /etc/makepkg.conf
