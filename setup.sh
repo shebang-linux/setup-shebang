2aaf9b8c8c3d38c469fcf7dbb40aa6703518785f
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-09-15 08:32:51 +0000

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -40,6 +40,7 @@ echo -e '# Default mirrors
 Server = https://mirrors.rit.edu/artixlinux/$repo/os/$arch
 Server = https://mirrors.dotsrc.org/artix-linux/repos/$repo/os/$arch' >/etc/pacman.d/mirrorlist
 sed -i -e '/^HoldPkg/a XferCommand = /sbin/curl -fSL --proto '\''=https'\'' --tlsv1.3 --progress-bar -o %o %u' /etc/pacman.conf
+sed -i -e 's/^SigLevel.*/Siglevel = Required DatabaseOptional TrustedOnly/g' /etc/pacman.conf
 sed -i -e s"/\#CleanMethod.*/CleanMethod = KeepCurrent/"g /etc/pacman.conf
 
 # Dependencies
