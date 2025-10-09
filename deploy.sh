4054415b53ad21b483bd5a3f569ba1d9f7cdebb3
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-10-09 20:59:29 +0000

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -447,6 +447,7 @@ net.ipv4.conf.all.accept_source_route=0
 net.ipv4.conf.all.arp_evict_nocarrier=1
 net.ipv4.conf.all.arp_ignore=1
 net.ipv4.conf.all.log_martians=0
+net.ipv4.tcp_notsent_lowat=131072
 net.ipv4.udp_rmem_min=8192
 net.ipv4.udp_wmem_min=8192
 net.ipv4.udp_early_demux=1
