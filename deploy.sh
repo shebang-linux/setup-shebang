f309ab7e28d582ee2ae04868ea91d75c35a8ebcf
YurinDoctrine <43861005+YurinDoctrine@users.noreply.github.com>
2025-10-15 08:57:01 +0000

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ yes "$ROOT_PASSWORD" | passwd
 echo -e '# Default mirrors
 Server = https://mirrors.rit.edu/artixlinux/$repo/os/$arch
 Server = https://mirrors.dotsrc.org/artix-linux/repos/$repo/os/$arch' >/etc/pacman.d/mirrorlist
-sed -i -e '/^HoldPkg/a XferCommand = /sbin/curl -SL --proto=https --tlsv1.3 --progress-bar -o %o %u' /etc/pacman.conf
+sed -i -e '/^HoldPkg/a XferCommand = /sbin/curl -fSL --proto=https --tlsv1.3 --progress-bar -o %o %u' /etc/pacman.conf
 sed -i -e '/#DisableSandbox/a DisableDownloadTimeout' /etc/pacman.conf
 sed -i -e 's/^SigLevel.*/SigLevel = Required DatabaseOptional TrustedOnly/g' /etc/pacman.conf
 sed -i -e s"/\#VerbosePkgLists/VerbosePkgLists/"g /etc/pacman.conf
